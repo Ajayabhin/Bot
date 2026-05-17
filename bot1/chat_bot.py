40cfb3e7214e8777b75052f26f9c1fb2298e8dc3
Ajay Abhinav <ajayabhinav7103@gmail.com>
2026-05-17 17:23:28 +0530

    bot 1 to qwen

--- bot1/chat_bot.py
+++ bot1/chat_bot.py
@@ -1,18 +1,24 @@
 from flask import Flask, request, jsonify, render_template
 from dotenv import load_dotenv
 from huggingface_hub import InferenceClient
+from transformers import pipeline
 import os
 
 load_dotenv("../.env")
 
 app = Flask(__name__)
 
-
-
+# client = InferenceClient(
+#     provider="fireworks-ai",
+#     api_key=os.getenv("CHATBOT_API_KEY")
+# )
 client = InferenceClient(
-    provider="fireworks-ai",
-    api_key=os.getenv("CHATBOT_API_KEY")
-)
+            api_key=os.getenv("CHATBOT_API_KEY"),
+        )
+
+pipe = pipeline("text-generation", model="Qwen/Qwen2.5-0.5B-Instruct",
+    trust_remote_code=True)
+       
 
 @app.route("/")
 def home():
@@ -25,15 +31,42 @@ def chat():
          
         user_message = request.json["message"]
 
-        response = client.chat.completions.create(
-            model="meta-llama/Llama-3.1-8B-Instruct",
-            messages=[
-                {   
-                    "role": "user", 
-                    "content": user_message}
-            ],            
-            max_tokens=100
+        # response = client.chat.completions.create(
+        #     model="meta-llama/Llama-3.1-8B-Instruct",
+        #     messages=[
+        #         {   
+        #             "role": "user", 
+        #             "content": user_message}
+        #     ],            
+        #     max_tokens=100
+        # )
+
+        # messages = [
+        #         {"role": "user", "content": user_message},
+        #     ]
+        
+        prompt = f"User: {user_message}\nAssistant:"
+        result = pipe(
+            prompt,
+            max_new_tokens=100,
+            clean_up_tokenization_spaces=False
         )
+
+        # pipe(result)  
+
+             
+        # completion = client.chat.completions.create(
+        #     model="deepseek-ai/DeepSeek-V4-Pro:novita",
+        #     messages=[
+        #         {
+        #             "role": "user",
+        #             "content": user_message
+        #         }
+        #     ],
+        #     max_tokens=100
+        # )
+
+        print(result)
 
         #bot_reply = response  # returns a string dir
        
@@ -46,9 +79,9 @@ def chat():
         #         }
         #     ]
         # )
-
-        bot_reply = response.choices[0].message.content
-
+        generated_text = result[0]["generated_text"]
+        bot_reply = generated_text.split("Assistant:")[-1].strip()
+        # completion.choices[0].message.content
         return jsonify({
             "reply": bot_reply
         })
